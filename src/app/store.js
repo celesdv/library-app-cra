264740bef83b52e5303c026f11b35627308031a0
slavks <exeherrera96@gmail.com>
2022-10-04 15:08:48 -0300

    :sparkles: New structure

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import bookReducer from '../features/book/bookSlice'
+
+export const store = configureStore({
+  reducer: {
+    book: bookReducer,
+  },
+})
