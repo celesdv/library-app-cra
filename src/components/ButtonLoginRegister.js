ba8bcbd653cca3aa44ceb573e926278de7dd8146
Ezequiel Hornos <44302487+ezequielh14@users.noreply.github.com>
2022-10-17 14:57:33 -0300

    :sparkles: Update: Design Update

--- src/components/ButtonLoginRegister.js
+++ src/components/ButtonLoginRegister.js
@@ -11,7 +11,7 @@ const ButtonLoginRegister = () => {
         <>
             {currentUser ? (
                 <button
-                    className="group absolute top-0 right-0 mx-6 my-6 cursor-pointer rounded-full border border-rose-700 bg-slate-100 px-2 py-1 hover:bg-rose-800 focus:outline-none focus:ring-1 focus:ring-rose-300"
+                    className="group absolute top-0 right-0 mx-6 my-6 cursor-pointer rounded-full border border-rose-700 bg-slate-100 px-6 py-2 hover:bg-rose-800 focus:outline-none focus:ring-1 focus:ring-rose-300"
                     onClick={logoutUser}
                 >
                     <span className="text-center text-lg font-semibold text-rose-700 group-hover:text-white">
@@ -20,7 +20,7 @@ const ButtonLoginRegister = () => {
                 </button>
             ) : (
                 <button
-                    className="group absolute top-0 right-0 mx-6 my-6 cursor-pointer rounded-full border border-teal-700 bg-slate-100 px-2 py-1 hover:bg-teal-800 focus:outline-none focus:ring-1 focus:ring-teal-300"
+                    className="group absolute top-0 right-0 mx-6 my-6 cursor-pointer rounded-full border border-teal-700 bg-slate-100 px-6 py-2 hover:bg-teal-800 focus:outline-none focus:ring-1 focus:ring-teal-300"
                     onClick={() => {
                         navigate("/login");
                     }}
